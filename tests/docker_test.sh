de7a4d6b4f80420c313921c6fac72f115662c179
Phil Ewels <phil.ewels@scilifelab.se>
2016-11-25 09:45:29 +0100

    Tidied up configs. Changed test data dl to uppmax.

--- tests/docker_test.sh
+++ tests/docker_test.sh
@@ -9,13 +9,13 @@ else
     script_path=$1
 fi
 
-curl --version >/dev/null 2>&1 || { echo >&2 "I require curl, but it's not installed.  Aborting."; exit 1; }
-tar --version >/dev/null 2>&1 || { echo >&2 "I require tar, but it's not installed.  Aborting."; exit 1; }
+curl --version >/dev/null 2>&1 || { echo >&2 "I require curl, but it's not installed. Aborting."; exit 1; }
+tar --version >/dev/null 2>&1 || { echo >&2 "I require tar, but it's not installed. Aborting."; exit 1; }
 docker -v >/dev/null 2>&1 || { echo >&2 "I require docker, but it's not installed. Visit https://www.docker.com/products/overview#/install_the_platform  ."; exit 1; }
 nextflow -v >/dev/null 2>&1 || { echo >&2 "I require nextflow, but it's not installed. If you hava Java, run 'curl -fsSL get.nextflow.io | bash'. If not, install Java."; exit 1; }
 
 echo "Downloading test set..."
-curl https://s3-eu-west-1.amazonaws.com/ngi-rnaseq/ngi-rna_test_set.tar.bz2 > /tmp/ngi-rna_test_set.tar.bz2
+curl https://export.uppmax.uu.se/b2013064/test-data/ngi-rna_test_set.tar.bz2 > /tmp/ngi-rna_test_set.tar.bz2
 echo "Done"
 if [ -d /tmp/ngi-rna_test_set ]
 then

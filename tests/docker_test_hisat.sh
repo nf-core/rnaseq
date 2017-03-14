4f9f42ea4e53cd53241d081e4603a3613dce3ef7
Phil Ewels <phil.ewels@scilifelab.se>
2017-03-14 11:48:54 +0100

    Made test scripts a little more friendly for repeated local testing.

--- tests/docker_test_hisat.sh
+++ tests/docker_test_hisat.sh
@@ -4,9 +4,15 @@ script_path="../main.nf"
 if [ -z $1]
 then
     echo "No argument given, going to try to run ../main.nf"
-
 else
     script_path=$1
+fi
+
+data_path="/tmp/"
+if [ -d "./test_data" ]
+then
+    data_path="./test_data/"
+    echo "Found data directory in current working directory, using ./test_data/"
 fi
 
 curl --version >/dev/null 2>&1 || { echo >&2 "I require curl, but it's not installed. Aborting."; exit 1; }
@@ -14,18 +20,17 @@ tar --version >/dev/null 2>&1 || { echo >&2 "I require tar, but it's not install
 docker -v >/dev/null 2>&1 || { echo >&2 "I require docker, but it's not installed. Visit https://www.docker.com/products/overview#/install_the_platform  ."; exit 1; }
 nextflow -v >/dev/null 2>&1 || { echo >&2 "I require nextflow, but it's not installed. If you hava Java, run 'curl -fsSL get.nextflow.io | bash'. If not, install Java."; exit 1; }
 
-echo "Downloading test set..."
-curl https://export.uppmax.uu.se/b2013064/test-data/ngi-rna_test_set.tar.bz2 > /tmp/ngi-rna_test_set.tar.bz2
-echo "Done"
-if [ -d /tmp/ngi-rna_test_set ]
+if [ -d "${data_path}/ngi-rna_test_set" ]
 then
-    echo "Removing previous test set..."
-    rm -r /tmp/ngi-rna_test_set
+    echo "Found existing test set, using ${data_path}/ngi-rna_test_set"
+else
+    echo "Downloading test set..."
+    curl https://export.uppmax.uu.se/b2013064/test-data/ngi-rna_test_set.tar.bz2 > ${data_path}/ngi-rna_test_set.tar.bz2
+    echo "Unpacking test set..."
+    tar xvjf ${data_path}/ngi-rna_test_set.tar.bz2 -C ${data_path}
     echo "Done"
 fi
-echo "Unpacking test set..."
-tar xvjf /tmp/ngi-rna_test_set.tar.bz2 -C /tmp
-echo "Done"
+
 echo "Starting nextflow..."
-nextflow run $script_path -profile docker_test --aligner hisat2 --reads '/tmp/ngi-rna_test_set/*.fastq.gz'
+nextflow run $script_path -resume -profile docker_test --aligner hisat2 --reads "${data_path}/ngi-rna_test_set/*.fastq.gz"
 

5bc51c91a25aa3e2f71af8bab6d321c1b06eb470
Rickard Hammarén <rickard.hammaren@scilifelab.se>
2017-03-24 16:31:54 +0100

    made logger work

--- bin/merge_featurecounts.py
+++ bin/merge_featurecounts.py
@@ -6,55 +6,54 @@ import re
 import logging
 from collections import defaultdict
 
-
 def merge_featureCounts(dest_dir,out_file,input_files):
+   logger = logging.getLogger(__name__)
+   logger.addHandler(logging.StreamHandler())
+   logger.setLevel(logging.INFO)
+   
    table_dict=defaultdict(dict)
    sample_names=[]
-   gene_list=[]
+   genes=set()
    for input_file in input_files:
-       if not gene_list:
-           first_file=True
-       print"Reading from {}".format(input_file)
+       logger.info("Reading from {}".format(input_file))
        sample_name=os.path.basename(input_file)
        sample_name= sample_name.replace("Aligned.sortedByCoord.out_gene.featureCounts.txt","")
        sample_names.append(sample_name)
        table_dict[sample_name]=dict()
      
        with open(input_file, 'r') as f:
+           f.readline()
+           f.readline()
            for line in f:
-               if not line.startswith('E'):
-                   continue
                #save the genes to a list for the first file
-               if first_file:
-                   gene_list.append(line.split('\t')[0])
                line_info=line.split('\t')
                gene=line_info[0]
+               genes.add(gene)
                try:
                    gene_count = int(line_info[-1].rstrip())
                    table_dict[sample_name][gene] = gene_count
                except TypeError:
-                       logging.info("Detected discrepancy in {}  line {}".format(input_file, line))
+                       logger.warning("Detected discrepancy in {}  line {}".format(input_file, line))
                 
                table_dict[sample_name][gene]=gene_count
-               #Done with first file
-               first_file=False
 
    #write Output
-   logging.info("Writing to file {}".format(out_file))
+   logger.info("Writing to file {}".format(out_file))
    with open(out_file, 'w') as f:
        #Generate header
        line_to_write="ENSEMBL_ID"
        sample_names.sort()
        for sample_name in sample_names:
-           line_to_write+=('\t'+ sample_name)
+           line_to_write+=('\t{}'.format(sample_name))
        line_to_write += "\n"
        f.write(line_to_write)    
        #Write the rest of the lines
+       gene_list=list(genes)
        gene_list.sort()
        for gene in gene_list:
            line_to_write=gene
            for sample_name in sample_names:
-               line_to_write += ('\t'+table_dict[sample_name][gene])
+               line_to_write += ('\t{}'.format(table_dict[sample_name][gene]))
            line_to_write += "\n"
            f.write(line_to_write)
 

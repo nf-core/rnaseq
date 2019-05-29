67c5484b568ad854e6a46542389de513227d1e49
Olga Botvinnik <olga.botvinnik@gmail.com>
2019-05-29 09:52:01 -0700

    Add fasta2gtf

---
+++ bin/fasta2gtf.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+"""
+Read a custom fasta file and create a custom GTF containing each entry
+"""
+import argparse
+from itertools import groupby
+import logging
+import os
+
+# Create a logger
+logging.basicConfig(format='%(name)s - %(asctime)s %(levelname)s: %(message)s')
+logger = logging.getLogger(__file__)
+logger.setLevel(logging.INFO)
+
+
+def fasta_iter(fasta_name):
+    """
+    modified from Brent Pedersen
+    Correct Way To Parse A Fasta File In Python
+    given a fasta file. yield tuples of header, sequence
+
+    Fasta iterator from https://www.biostars.org/p/710/#120760
+    """
+    with open(fasta_name) as fh:
+        # ditch the boolean (x[0]) and just keep the header or sequence since
+        # we know they alternate.
+        faiter = (x[1] for x in groupby(fh, lambda line: line[0] == ">"))
+        for header in faiter:
+            # drop the ">"
+            headerStr = header.__next__()[1:].strip()
+
+            # join all sequence lines to one.
+            seq = "".join(s.strip() for s in faiter.__next__())
+
+            yield (headerStr, seq)
+
+
+def fasta2gtf(fasta, output):
+    fiter = fasta_iter(fasta)
+    # GTF output lines
+    lines = []
+    attributes = 'gene_id "{name}_transgene"; gene_name "{name}_transgene"; transcript_id "{name}_transgene";\n'
+    line_template = "{name}\ttransgene\texon\t1\t{length}\t.\t+\t.\t" + attributes
+
+    for ff in fiter:
+        name, seq = ff
+        # Add +1 for length because GTFs are 1-based
+        length = len(seq) + 1
+        line = line_template.format(name=name, length=length)
+        lines.append(line)
+
+    with open(output, 'w') as f:
+        f.write(''.join(lines))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="""Convert a custom fasta (e.g. transgene) to a GTF annotation.""")
+    parser.add_argument("fasta", type=str, help="Custom transgene sequence")
+    parser.add_argument("-o", "--output", dest='output', default='transgenes.gtf', type=str, help="gene annotation GTF output")
+    args = parser.parse_args()
+    fasta2gtf(args.fasta, args.output)

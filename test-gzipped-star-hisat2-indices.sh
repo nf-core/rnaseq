72409c4ac4f3abf682d1e475f7584d19f8fc55d8
Olga Botvinnik <olga.botvinnik@gmail.com>
2019-09-04 10:15:59 -0700

    Add script for testing gzipped references-specific things on Travis CI

---
+++ test-gzipped-star-hisat2-indices.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -ev
+bundle exec rake:units
+if [ "${SUITE}" = "test_gz" ]; then
+  # Run without premade STAR indices - made within nextflow
+  nextflow run ${TRAVIS_BUILD_DIR} -profile $SUITE,docker --star_index false
+
+  # Run with HiSat2 and no premade indices -- do-it-yourself reference
+  nextflow run ${TRAVIS_BUILD_DIR} -profile $SUITE,docker --aligner hisat2 --hisat2_index false
+fi

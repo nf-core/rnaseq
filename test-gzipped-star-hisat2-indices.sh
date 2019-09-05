456fc8d5d82acd6fb35f1d3f78e88da13817711d
Olga Botvinnik <olga.botvinnik@gmail.com>
2019-09-05 06:18:27 -0700

    remove ruby cruftc

--- test-gzipped-star-hisat2-indices.sh
+++ test-gzipped-star-hisat2-indices.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -ev
-bundle exec rake:units
 if [ "${SUITE}" = "test_gz" ]; then
   # Run without premade STAR indices - made within nextflow
   nextflow run ${TRAVIS_BUILD_DIR} -profile $SUITE,docker --star_index false --skipQC

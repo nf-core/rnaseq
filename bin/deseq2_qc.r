fa39dcdbab5555960eee43be9597e135c6f4f1c1
Gavin Kelly <gavin.kelly@crick.ac.uk>
2020-11-02 16:14:05 +0000

    Embed VST assay into DESeq object.
    Variance-stabilised matrix is now embedded in the single DESeqDataSet
    object, so that any user-based re-organisation of the object is
    mirrored in the vst matrix automatically.
    
    Also parametrise number of gene taken into account for PCA - author
    prefers all genes, as it is important to detect synchronised but
    individually small-amplitude signals at this stage, to diagnose batch
    effect problems.  But introduce a parameter 'ntop' so this can be
    over-ridden by user.
    
    Detect which PCs are explained most by the user-supplied
    'condition'. Plot the most 'experimentally interesting' PCs, as an addition
    to the 'blind' PC1 and PC2.

--- bin/deseq2_qc.r
+++ bin/deseq2_qc.r
@@ -37,6 +37,7 @@ option_list <- list(
     make_option(c("-o", "--outdir"        ), type="character", default='./'    , metavar="path"   , help="Output directory."                                                                      ),
     make_option(c("-p", "--outprefix"     ), type="character", default='deseq2', metavar="string" , help="Output prefix."                                                                         ),
     make_option(c("-v", "--vst"           ), type="logical"  , default=FALSE   , metavar="boolean", help="Run vst transform instead of rlog."                                                     ),
+    make_option(c("-n", "--ntop"          ), type="integer"  , default=NA      , metavar="integer", help="Limit PCA to most variable genes. Defaults to all genes, to detect batch effects."      ),
     make_option(c("-c", "--cores"         ), type="integer"  , default=1       , metavar="integer", help="Number of cores."                                                                       )
 )
 
@@ -84,13 +85,23 @@ if (file.exists(DDSFile) == FALSE) {
     dds     <- DESeqDataSetFromMatrix(countData=round(counts), colData=coldata, design=~ condition)
     dds     <- DESeq(dds, parallel=TRUE, BPPARAM=MulticoreParam(opt$cores))
     if (!opt$vst) {
-        rld <- rlog(dds)
+      vst_name <- "rlog"
+      rld <- rlog(dds)
     } else {
-        rld <- varianceStabilizingTransformation(dds)
+      vst_name <- "vst"
+      rld <- varianceStabilizingTransformation(dds)
     }
-    save(dds,rld,file=DDSFile)
+    assay(dds, vst_name) <- assay(rld)
+    save(dds,file=DDSFile)
 } else {
     load(DDSFile)
+    vst_name <- intersect(assayNames(dds), c("vst", "rlog"))
+    if (length(vst_name)==0) { # legacy might mean vst was saved as a separate object called rld
+      vst_name <- "loaded_rld"
+      assay(dds, vst_name) <- assay(rld)
+    } else {
+      vst_name==vst_name[1]
+    }
 }
 
 ################################################
@@ -99,23 +110,101 @@ if (file.exists(DDSFile) == FALSE) {
 ################################################
 ################################################
 
+##' PCA pre-processeor
+##'
+##' Generate all the necessary information to plot PCA from a DESeq2 object
+##' in which an assay containing a variance-stabilised matrix of counts is
+##' stored. Copied from DESeq2::plotPCA, but with additional ability to
+##' say which assay to run the PCA on, and adds an assessment of how well
+##' each PC explains the experimental grouping of the data.
+##' 
+##' @param object The DESeq2DataSet object.
+##' @param intgroup interesting groups: a character vector of names in 'colData(x)' to use for grouping.
+##' @param ntop number of top genes to use for principla components, selected by highest row variance.
+##' @param assay the name or index of the assay that stores the variance-stabilised data.
+##' @return A data.frame containing the projected data alongside the grouping columns.
+##' A 'percentVar' attribute is set which includes the percentage of variation each PC explains,
+##' and additionally how much the variation within that PC is explained by the grouping variable.
+##' @author Gavin Kelly
+plotPCA_vst <- function (object, intgroup = "condition", ntop = 500, assay=length(assays(object))) {
+  rv <- rowVars(assay(object, assay))
+  select <- order(rv, decreasing = TRUE)[seq_len(min(ntop, 
+                                                    length(rv)))]
+  pca <- prcomp(t(assay(object, assay)[select, ]))
+  percentVar <- pca$sdev^2/sum(pca$sdev^2)
+  if (!all(intgroup %in% names(colData(object)))) {
+    stop("the argument 'intgroup' should specify columns of colData(dds)")
+  }
+  intgroup.df <- as.data.frame(colData(object)[, intgroup, 
+                                              drop = FALSE])
+  group <- if (length(intgroup) > 1) {
+    factor(apply(intgroup.df, 1, paste, collapse = ":"))
+  }  else {
+    colData(object)[[intgroup]]
+  }
+  d <- cbind(pca$x, group = group, 
+            intgroup.df,
+            name = colnames(object))
+  percentFrame <- data.frame(PC=seq(along=percentVar),
+                            percentVar=100*percentVar,
+                            groupR=0.0)
+  for (ipc in seq(along=percentVar)) {
+    fit1 <- lm(pca$x[,ipc]  ~ group)
+    percentFrame$groupR[ipc] <- 100*summary(fit1)$r.squared
+  }
+  attr(d, "percentVar") <- percentFrame
+  return(d)
+}
+
 PlotFile <- paste(opt$outprefix,".plots.pdf",sep="")
 if (file.exists(PlotFile) == FALSE) {
     pdf(file=PlotFile,onefile=TRUE,width=7,height=7)
 
     ## PCA
-    pca.data <- DESeq2::plotPCA(rld,intgroup=c("condition"),returnData=TRUE)
-    percentVar <- round(100 * attr(pca.data, "percentVar"))
-    plot <- ggplot(pca.data, aes(PC1, PC2, color=condition)) +
+    if (is.na(opt$ntop)) {
+      ntop <- Inf
+    } else {
+      ntop <- c(Inf, ntop)
+    }
+    for (n_top_var in ntop) {
+      pca.data <- plotPCA_vst(dds, assay=vst_name,intgroup=c("condition"),ntop=n_top_var)
+      percentVar <- round(attr(pca.data, "percentVar")$percentVar)
+      plot_subtitle <- ifelse(n_top_var==Inf, "All genes", paste("Top", n_top_var, "genes"))
+      pl <- ggplot(pca.data, aes(PC1, PC2, color=condition)) +
             geom_point(size=3) +
             xlab(paste0("PC1: ",percentVar[1],"% variance")) +
-            ylab(paste0("PC2: ",percentVar[2],"% variance")) +
+        ylab(paste0("PC2: ",percentVar[2],"% variance")) +
+        labs(title = paste0("First PCs on ", vst_name, "-transformed data"),
+              subtitle = plot_subtitle) + 
             theme(panel.grid.major = element_blank(),
                   panel.grid.minor = element_blank(),
                   panel.background = element_blank(),
                   panel.border = element_rect(colour = "black", fill=NA, size=1))
-    print(plot)
-
+      print(pl)
+      pl <- ggplot(attr(pca.data, "percentVar"), aes(x=PC, y=percentVar)) +
+        geom_line(aes(colour="explained by PC")) +
+        geom_line(aes(y=groupR, colour="of PC explained by condition")) +
+        scale_x_continuous(breaks=plotFrame$PC, minor_breaks=NULL)  +
+        labs(title="Diagnostics of PCs",
+             subtitle=plot_subtitle,
+             x="Component", y="Percentage explaned",
+             colour="Percentage variation") +
+        theme_bw() +
+        theme(legend.position=c(0.75, 0.8))
+      print(pl)
+      pc_r <- order(attr(pca.data, "percentVar")$groupR, decreasing=TRUE)
+      pl <- ggplot(pca.data, aes_string(paste0("PC", pc_r[1]), paste0("PC", pc_r[2]), color="condition")) +
+        geom_point(size=3) +
+        xlab(paste0("PC", pc_r[1], ": ",percentVar[pc_r[1]],"% variance")) +
+        ylab(paste0("PC", pc_r[2], ": ",percentVar[pc_r[2]],"% variance")) +
+        labs(title = paste0("Group-Explanatory PCs of ", vst_name, "-tranformed data"),
+             subtitle = plot_subtitle) + 
+        theme(panel.grid.major = element_blank(),
+              panel.grid.minor = element_blank(),
+              panel.background = element_blank(),
+              panel.border = element_rect(colour = "black", fill=NA, size=1))
+    } # at end of loop, we'll be using the user-defined ntop if any, else all genes
+    
     ## WRITE PC1 vs PC2 VALUES TO FILE
     pca.vals           <- pca.data[,1:2]
     colnames(pca.vals) <- paste(colnames(pca.vals),paste(percentVar,'% variance',sep=""), sep=": ")
@@ -123,7 +212,7 @@ if (file.exists(PlotFile) == FALSE) {
     write.table(pca.vals,file=paste(opt$outprefix,".pca.vals.txt",sep=""),row.names=FALSE,col.names=TRUE,sep="\t",quote=TRUE)
 
     ## SAMPLE CORRELATION HEATMAP
-    sampleDists      <- dist(t(assay(rld)))
+    sampleDists      <- dist(t(assay(rld, vst_name)))
     sampleDistMatrix <- as.matrix(sampleDists)
     colors           <- colorRampPalette( rev(brewer.pal(9, "Blues")) )(255)
     pheatmap(sampleDistMatrix,clustering_distance_rows=sampleDists,clustering_distance_cols=sampleDists,col=colors)

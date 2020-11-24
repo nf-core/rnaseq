3637e74b42d1e3d61f3c7328a1a876932c512535
drpatelh <drhpatel@gmail.com>
2020-11-24 12:29:34 +0000

    Fix AWS batch assert issues

--- lib/Checks.groovy
+++ lib/Checks.groovy
@@ -6,12 +6,12 @@ class Checks {
 
     static void aws_batch(workflow, params) {
         if (workflow.profile.contains('awsbatch')) {
-            assert !params.awsqueue || !params.awsregion : "Specify correct --awsqueue and --awsregion parameters on AWSBatch!"
+            if (!params.awsqueue || !params.awsregion) exit 1, 'Specify correct --awsqueue and --awsregion parameters on AWSBatch!'
             // Check outdir paths to be S3 buckets if running on AWSBatch
             // related: https://github.com/nextflow-io/nextflow/issues/813
-            assert !params.outdir.startsWith('s3:') : "Outdir not on S3 - specify S3 Bucket to run on AWSBatch!"
+            if (!params.outdir.startsWith('s3:')) exit 1, 'Outdir not on S3 - specify S3 Bucket to run on AWSBatch!'
             // Prevent trace files to be stored on S3 since S3 does not support rolling files.
-            assert params.tracedir.startsWith('s3:') :  "Specify a local tracedir or run without trace! S3 cannot be used for tracefiles."
+            if (params.tracedir.startsWith('s3:')) exit 1, 'Specify a local tracedir or run without trace! S3 cannot be used for tracefiles.'
         }
     }
 

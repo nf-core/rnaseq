0c372f73c94ad13cd61512f19dbbeb4c3e46d314
drpatelh <drhpatel@gmail.com>
2020-11-24 13:04:34 +0000

    Invert logic

--- lib/Checks.groovy
+++ lib/Checks.groovy
@@ -5,13 +5,14 @@
 class Checks {
 
     static void aws_batch(workflow, params) {
-        if (workflow.profile.contains('awsbatch')) {
-            if (!params.awsqueue || !params.awsregion) exit 1, 'Specify correct --awsqueue and --awsregion parameters on AWSBatch!'
+        //if (workflow.profile.contains('awsbatch')) {
+        if (workflow.profile.contains('crick')) {
+            assert (params.awsqueue || params.awsregion) : "Specify correct --awsqueue and --awsregion parameters on AWSBatch!"
             // Check outdir paths to be S3 buckets if running on AWSBatch
             // related: https://github.com/nextflow-io/nextflow/issues/813
-            if (!params.outdir.startsWith('s3:')) exit 1, 'Outdir not on S3 - specify S3 Bucket to run on AWSBatch!'
+            assert params.outdir.startsWith('s3:')       : "Outdir not on S3 - specify S3 Bucket to run on AWSBatch!"
             // Prevent trace files to be stored on S3 since S3 does not support rolling files.
-            if (params.tracedir.startsWith('s3:')) exit 1, 'Specify a local tracedir or run without trace! S3 cannot be used for tracefiles.'
+            assert !params.tracedir.startsWith('s3:')    :  "Specify a local tracedir or run without trace! S3 cannot be used for tracefiles."
         }
     }
 

1ca342cbe1b7fecb98fa26d0190d4ff1515cfbdf
drpatelh <drhpatel@gmail.com>
2020-12-07 14:38:30 +0000

    Minor updates

--- lib/Checks.groovy
+++ lib/Checks.groovy
@@ -48,8 +48,8 @@ class Checks {
     static void genome_exists(params, log) {
         if (params.genomes && params.genome && !params.genomes.containsKey(params.genome)) {
             log.error "=============================================================================\n" +
-                      "  Genome '${params.genome}' was not found in any config files made available to the pipeline.\n" +
-                      "  Currently, the available genomes are:\n" +
+                      "  Genome '${params.genome}' not found in any config files provided to the pipeline.\n" +
+                      "  Currently, the available genome keys are:\n" +
                       "  ${params.genomes.keySet().join(", ")}\n" +
                       "============================================================================="
             System.exit(0)

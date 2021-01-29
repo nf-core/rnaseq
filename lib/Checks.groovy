d8b287ec47460267d44c8c4cf1f79f355a733658
Maxime Garcia <max.u.garcia@gmail.com>
2021-01-29 16:41:53 +0100

    Remove Readcube link

--- lib/Checks.groovy
+++ lib/Checks.groovy
@@ -71,7 +71,6 @@ class Checks {
                "  https://doi.org/10.5281/zenodo.1400710\n\n" +
                "* The nf-core framework\n" +
                "  https://dx.doi.org/10.1038/s41587-020-0439-x\n" +
-               "  https://rdcu.be/b1GjZ\n\n" +
                "* Software dependencies\n" +
                "  https://github.com/${workflow.manifest.name}/blob/master/CITATIONS.md"
     }

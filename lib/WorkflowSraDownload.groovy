b79296e56c60bdcb80f385e280f0fced28b720c0
Harshil Patel <drpatelhh@gmail.com>
2021-05-04 10:20:06 +0100

    Update SRA download message due to samplesheet change

--- lib/WorkflowSraDownload.groovy
+++ lib/WorkflowSraDownload.groovy
@@ -3,17 +3,15 @@
  */
 
 class WorkflowSraDownload {
-    
+
     // Print a warning after SRA download has completed
     public static void sraDownloadWarn(log) {
         log.warn "=============================================================================\n" +
                  "  Please double-check the samplesheet that has been auto-created using the\n" +
                  "  public database ids provided via the '--public_data_ids' parameter.\n\n" +
-                 "  Public databases don't reliably hold information such as replicate identifiers,\n" +
-                 "  or strandedness information.\n\n" +  
                  "  All of the sample metadata obtained from the ENA has been appended\n" +
                  "  as additional columns to help you manually curate the samplesheet before\n" +
-                 "  you run the pipeline.\n" +
+                 "  you run the main workflow(s) in the pipeline.\n" +
                  "==================================================================================="
     }
 }

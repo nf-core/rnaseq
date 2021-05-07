4bf388848554b54de0fa3ea77eedea119b15d3e5
Harshil Patel <drpatelhh@gmail.com>
2021-05-07 09:40:43 +0100

    Remove tracedir assertion for awsbatch profile

--- lib/NfcoreTemplate.groovy
+++ lib/NfcoreTemplate.groovy
@@ -11,12 +11,10 @@ class NfcoreTemplate {
      */
     public static void awsBatch(workflow, params) {
         if (workflow.profile.contains('awsbatch')) {
+            // Check params.awsqueue and params.awsregion have been set if running on AWSBatch
             assert (params.awsqueue && params.awsregion) : "Specify correct --awsqueue and --awsregion parameters on AWSBatch!"
             // Check outdir paths to be S3 buckets if running on AWSBatch
-            // related: https://github.com/nextflow-io/nextflow/issues/813
             assert params.outdir.startsWith('s3:')       : "Outdir not on S3 - specify S3 Bucket to run on AWSBatch!"
-            // Prevent trace files to be stored on S3 since S3 does not support rolling files.
-            assert !params.tracedir.startsWith('s3:')    :  "Specify a local tracedir or run without trace! S3 cannot be used for tracefiles."
         }
     }
 

01e05f74a96d86d9ea4fcc8c8ee9655420ada095
Jonathan Manning <jonathan.manning@seqera.io>
2023-11-14 10:11:43 +0000

    [skip ci] more post-review tidy-up to GTF filtering script

--- bin/filter_gtf.py
+++ bin/filter_gtf.py
@@ -1,106 +1,55 @@
 #!/usr/bin/env python
-from __future__ import print_function
 import logging
-from itertools import groupby
 import argparse
 import re
+import statistics
+from typing import Set
 
 # Create a logger
 logging.basicConfig(format="%(name)s - %(asctime)s %(levelname)s: %(message)s")
-logger = logging.getLogger(__file__)
+logger = logging.getLogger("fasta_gtf_filter")
 logger.setLevel(logging.INFO)
 
-def is_header(line: str) -> bool:
-    """Returns True if the given line is a header line in a FASTA file."""
-    return line[0] == ">"
-
-
-def extract_fasta_seq_names(fasta_name: str) -> set:
-    """Extracts the sequence names from a FASTA file.
-
-    modified from Brent Pedersen
-    Correct Way To Parse A Fasta File In Python
-    given a fasta file. yield tuples of header, sequence
-    from https://www.biostars.org/p/710/
-
-    Args:
-      fasta_name: The path to the FASTA file.
-
-    Returns:
-      A set of the sequence names in the FASTA file.
-    """
-    seqnames = set()
+def extract_fasta_seq_names(fasta_name: str) -> Set[str]:
+    """Extracts the sequence names from a FASTA file."""
     with open(fasta_name) as fasta:
-        for line in fasta:
-            if line[0] == ">":
-                seqnames.add(line[1:].split(None, 1)[0])
-    return seqnames
+        return {line[1:].split(None, 1)[0] for line in fasta if line.startswith(">")}
 
+def tab_delimited(file: str) -> float:
+    """Check if file is tab-delimited and return median number of tabs."""
+    with open(file, "r") as f:
+        data = f.read(1024)
+        return statistics.median(line.count("\t") for line in data.split("\n"))
 
 def filter_gtf(fasta: str, gtf_in: str, gtf_in_genome_out: str, gtf_transcript_out: str) -> None:
-    """Extracts the genes in the genome from a GTF file.
-
-    Args:
-      fasta: The path to the FASTA file.
-      gtf_in: The path to the input GTF file.
-      gtf_in_genome_out: The path to the output GTF file with scaffolds filtered.
-      gtf_transcript_out: The path to the output GTF file with scaffolds
-          filtered and rows without transcript ID removed.
-
-    Raises:
-      ValueError: If no overlap is found or if the GTF file is not tab delimited.
-    """
-
-    def tab_delimited(file) -> float:
-    
-        import statistics. # put to the top
-
-        with open(file, "r") as f:
-            data = f.read(1024)
-            lines = data.split("\n")
-            # most lines should have 9 tab-separated columns
-            return statistics.median([line.count("\t") for line in lines])
-
-    num_sep = tab_delimited(gtf_in)
-    if num_sep != 8:
-        raise ValueError("No valid tab-delimited GTF file.")
+    """Filter GTF file based on FASTA sequence names."""
+    if tab_delimited(gtf_in) != 8:
+        raise ValueError("Invalid GTF file: Expected 8 tab-separated columns.")
 
     seq_names_in_genome = extract_fasta_seq_names(fasta)
     logger.info(f"Extracted chromosome sequence names from {fasta}")
-    logger.debug("All chromosome names: " + ", ".join(sorted(seq_names_in_genome)))
 
-    with open(gtf_in) as gtf, open(gtf_in_genome_out, "w") as out, open(
-        gtf_transcript_out, "w"
-    ) as out2:
-        line_count_all = 0
-        line_count_transcript = 0
-        for line in gtf:
-            if (
-                line.split("\t")[0] in seq_names_in_genome
-                and line.count("\t") == num_sep
-            ):
-                out.write(line)
-                line_count_all += 1
-                if re.search(r'transcript_id "([^"]+)"', line):
-                    out2.write(line)
-                    line_count_transcript += 1
-        if line_count_all == 0:
-            raise ValueError("No overlapping scaffolds found.")
-
-
+    try:
+        with open(gtf_in) as gtf, open(gtf_in_genome_out, "w") as out, open(gtf_transcript_out, "w") as out2:
+            line_count_all, line_count_transcript = 0, 0
+            for line in gtf:
+                if line.split("\t")[0] in seq_names_in_genome:
+                    out.write(line)
+                    line_count_all += 1
+                    if re.search(r'transcript_id "([^"]+)"', line):
+                        out2.write(line)
+                        line_count_transcript += 1
+            if line_count_all == 0:
+                raise ValueError("No overlapping scaffolds found.")
+    except IOError as e:
+        logger.error(f"File operation failed: {e}")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="""Filter GTF for various reasons""")
-    parser.add_argument("--gtf", type=str, help="GTF file")
-    parser.add_argument("--fasta", type=str, help="Genome fasta file")
-    parser.add_argument(
-        "-p",
-        "--prefix",
-        dest="prefix",
-        default="genes",
-        type=str,
-        help="Prefix for output GTF files",
-    )
+    parser = argparse.ArgumentParser(description="Filters a GTF file based on sequence names in a FASTA file.")
+    parser.add_argument("--gtf", type=str, required=True, help="GTF file")
+    parser.add_argument("--fasta", type=str, required=True, help="Genome fasta file")
+    parser.add_argument("--prefix", dest="prefix", default="genes", type=str, help="Prefix for output GTF files")
 
     args = parser.parse_args()
     filter_gtf(args.fasta, args.gtf, args.prefix + "_in_genome.gtf", args.prefix + "_with_transcript_ids.gtf")
+

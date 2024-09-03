65162d437492d78e583f4824826314d2852ff394
Jonathan Manning <jonathan.manning@seqera.io>
2024-09-03 18:23:34 +0100

    Bump modules

--- modules/nf-core/tximeta/tximport/templates/tximport.r
+++ modules/nf-core/tximeta/tximport/templates/tximport.r
@@ -153,7 +153,6 @@ if ("tx2gene" %in% names(transcript_info) && !is.null(transcript_info\$tx2gene))
     gi.s <- summarizeToGene(txi, tx2gene = tx2gene, countsFromAbundance = "scaledTPM")
 
     gene_info <- transcript_info\$gene[match(rownames(gi[[1]]), transcript_info\$gene[["gene_id"]]),]
-    rownames(gene_info) <- gene_info[["tx"]]
 
     col_data_frame <- DataFrame(coldata)
 

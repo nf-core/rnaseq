eada63ddbe0f3b03a0ecacd2f890c2584fbdd51e
maxulysse <max.u.garcia@gmail.com>
2024-09-13 09:57:36 +0200

    make snapshot versions pretty

--- tests/lib/UTILS.groovy
+++ tests/lib/UTILS.groovy
@@ -1,7 +1,7 @@
 // Function to remove Nextflow version from pipeline_software_mqc_versions.yml
 
 class UTILS {
-    public static String removeNextflowVersion(pipeline_software_mqc_versions) {
+    public static Object removeNextflowVersion(pipeline_software_mqc_versions) {
         def softwareVersions = path(pipeline_software_mqc_versions).yaml
         if (softwareVersions.containsKey("Workflow")) {
             softwareVersions.Workflow.remove("Nextflow")
